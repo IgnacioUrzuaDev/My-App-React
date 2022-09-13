65949e2b7ed6e463596adfb18844719160723d5a
Ignacio Urzua <ignacio.aum@gmail.com>
2022-09-13 01:02:53 -0300

    Added event to 'add to cart' button

--- src/Components/ItemDetail/ItemDetail.jsx
+++ src/Components/ItemDetail/ItemDetail.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import ItemCount from '../Counter/ItemCount'
 import '../ItemDetail/ItemDetail.css'
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
+
 
 const ItemDetail = ({item}) => {
+        const [showBtn, setshowBtn] = useState(true)
         const onAdd = (quantity) => {
                 console.log(quantity)
+                setshowBtn(false)
         }
+           
     return (
     <article>
         <h1>{item.title}</h1>
@@ -17,7 +23,7 @@ const ItemDetail = ({item}) => {
                 <p>{item.description}</p>
                 <p className="price">{item.price}</p>
                 <p>Stock: {item.stock}</p>
-                <ItemCount stock={item.stock} initial={0} onAdd={onAdd}/>
+                {showBtn ? <ItemCount stock={item.stock} initial={0} onAdd={onAdd}/> : <Link to={'/cart'} className='gotocartBtn' >Check your Cart</Link>}            
             </div>
         </div>
     </article>
